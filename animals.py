92e040213649f177b975283ebf3d391b0554db48
etecuatl <tecuatl.erick@gmail.com>
2024-11-13 19:29:06 -0500

    Adding colon

--- animals.py
+++ animals.py
@@ -2,6 +2,6 @@
 class Animal:
     '''This is the start of the the animal class for 3120.'''
 
-    def __init__(self,animal,kingdom,species)
+    def __init__(self,animal,kingdom,species):
         self.animal = animal
         self.kingdom = kingdom
