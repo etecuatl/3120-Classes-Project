eb9f188a9d351b6c01e6fa1ce839e2725e86cfe0
etecuatl <tecuatl.erick@gmail.com>
2024-11-13 19:24:07 -0500

    adding variables

--- animals.py
+++ animals.py
@@ -1,3 +1,7 @@
 
 class Animal:
     '''This is the start of the the animal class for 3120.'''
+
+    def __init__(self,animal,kingdom,species)
+        self.animal = animal
+        self.kingdom = kingdom
